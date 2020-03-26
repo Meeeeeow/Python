a6621efb52b06c261474d3792765a78e39223b72
Plunderer <kazutokun467@gmail.com>
2020-03-27 02:25:48 +0600

    data types,math functions basic

---
+++ deploy/data_types.py
@@ -0,0 +1,72 @@
+import math
+print(type(2.56))
+print(2**2)
+print(3 // 4)
+print(8 // 4)
+print(7 // 4)
+print(13 // 4)
+print(13%4)
+
+print(round(3.4))
+print(math.fabs(-20))
+print(math.sqrt(4))
+print(math.floor(3.5))
+print(math.fabs(-34))
+print(math.factorial(5))
+print(math.exp(2))
+print(math.pow(2,3))
+print(bin(5))
+print(int('0b101',2))
+print(hex(15))
+print(oct(9))
+print(int('0xf',16))
+value =5
+value+=2
+print(value)
+value*=2
+print(value)
+value /=2
+print(value)
+print(type(value))
+
+print('last namee')
+long_string='''
+Fuck
+you
+Play
+Dota
+'''+'more'
+print(long_string+' and more')
+first_name ='Drow'
+last_name = 'Ranger'
+full_name =first_name + ' '+last_name
+print(full_name)
+print('hello'+' 5')
+print(type(str(23.34)))
+a=int(str(23))
+print(bin(a))
+print("it's kinda sunny")
+print("\t  I wanna drink \"tea\"\nfuck you!")
+#formatted string
+name ='Bloodseeker'
+age =20
+print(f'hi {name}.You are {age} years old.')
+name ='Drow'
+age =20
+print(f'hi {name} . You are {age } years old')
+name='Phantom Asssasin'
+
+#string undex
+#start:stop:step_over
+
+print(name)
+print(name[0])
+print(name[0:11])
+print(name[0:3])
+print(name[6:])
+print(name[:2])
+print(name[0:11:1])
+print(name[0:11:2])
+print(name[-1])
+print(name[-2])
+print(name[::-1])
